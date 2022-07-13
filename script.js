dfd911460a3bbc1a6a7677199f376fc72f1e8e01
shushnikz <sushmithadevadiga997@gmail.com>
2022-07-13 18:28:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+//https://api.covid19api.com/summary
+//https://restcountries.com/v3.1/name/{name}
+
+let url=`https://api.covid19api.com/summary`
+
+
+
+async function updateMap(){
+  let data=await fetch(url)
+  let res=await data.json()
+//   console.log(res.Countries)
+
+  let list=res.Countries
+
+  list.map(async function(element){
+    console.log(element)
+
+    let data1=await fetch(`https://restcountries.com/v3.1/name/${element.Country}`)
+    let res1=await data1.json()
+    // console.log(res1)
+
+    let latitude=res1[0].latlng[0]
+    let longitude=res1[0].latlng[1]
+    console.log(latitude,longitude)
+
+    let cases=element.TotalDeaths
+    if(cases>255){
+        color="rgb(255,0,0)"
+    }
+    else if(cases>100 && cases<255){
+        color="rgb(197,62,56)"
+    }
+    else{
+        color=`rgb(${element.TotalDeaths},0,0)`;
+    }
+
+    //mark on the map or globe
+
+
+    new mapboxgl.Marker({
+        draggable: false,
+        color:color
+        })
+        .setLngLat([longitude, latitude])
+        .addTo(map);
+
+
+  })
+}
+updateMap()
+
+let interval=9000
+setInterval(updateMap,interval)
